58af806b7ff862b735afb3c23529f2cdc446361e
Samuel Woldegiorgies <samuel.woldegiorgies@icloud.com>
2026-04-01 20:02:00 +0300

    debug: add autoloader diagnostic logging

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -26,7 +26,10 @@ spl_autoload_register(function ($class) {
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
     
     if (file_exists($file)) {
+        error_log("Autoloader loading: " . $file);
         require_once $file;
+    } else {
+        error_log("Autoloader failed to find: " . $file);
     }
 });
 
