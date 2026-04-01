f3963e2447db08f6fc2156e57d84582af45beccf
Samuel Woldegiorgies <samuel.woldegiorgies@icloud.com>
2026-04-01 19:24:47 +0300

    chore: enable diagnostic error reporting and refine .htaccess for Namecheap deployment

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+// Enable full error reporting for production debugging on Namecheap/cPanel
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /**
  * AviatorTutor PHP Front Controller
